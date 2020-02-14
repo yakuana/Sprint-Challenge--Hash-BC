c318e2e83883d75053b99329ebade446df86c630
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-02-14 11:24:58 -0500

    WIP: hashtables ex1 complete and tested

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,11 +7,34 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    # ht = HashTable(16)
 
     """
     YOUR CODE HERE
     """
+    ht = HashTable(length)
+    count = 0
+    for i in weights:
+        print("IIII", i, count, hash_table_retrieve(ht, i))
+
+        if (hash_table_retrieve(ht, i) != None):
+            print("I and I", i + i, limit)
+            if (i + i == limit):
+                if (hash_table_retrieve(ht,i) > count):
+                    return (hash_table_retrieve(ht,i), count)
+                return (count,hash_table_retrieve(ht,i), count)
+
+        hash_table_insert(ht, i, count) 
+
+        if (hash_table_retrieve(ht, limit - i)):
+            print("Inside",hash_table_retrieve(ht, limit - i), count)  
+            if (hash_table_retrieve(ht,limit-i) > count):
+                print("Inside Inside", hash_table_retrieve(ht, limit - i), count)
+                return (hash_table_retrieve(ht,limit - i), count)
+            print("Inside Outside", count,hash_table_retrieve(ht, limit - i))
+            return (count,hash_table_retrieve(ht,limit - i), count)
+        print("COUNT", count)
+        count += 1
 
     return None
 
