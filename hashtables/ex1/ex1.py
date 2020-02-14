2df3c7b18ba94f3bd860904783cd26df505774e5
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-02-14 11:29:49 -0500

    WIP: removed unused functions in ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -14,25 +12,26 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     ht = HashTable(length)
     count = 0
+
     for i in weights:
         print("IIII", i, count, hash_table_retrieve(ht, i))
 
         if (hash_table_retrieve(ht, i) != None):
             print("I and I", i + i, limit)
             if (i + i == limit):
-                if (hash_table_retrieve(ht,i) > count):
-                    return (hash_table_retrieve(ht,i), count)
-                return (count,hash_table_retrieve(ht,i), count)
+                if (hash_table_retrieve(ht, i) > count):
+                    return (hash_table_retrieve(ht, i), count)
+                return (count,hash_table_retrieve(ht, i), count)
 
         hash_table_insert(ht, i, count) 
 
         if (hash_table_retrieve(ht, limit - i)):
             print("Inside",hash_table_retrieve(ht, limit - i), count)  
-            if (hash_table_retrieve(ht,limit-i) > count):
+            if (hash_table_retrieve(ht, limit - i) > count):
                 print("Inside Inside", hash_table_retrieve(ht, limit - i), count)
-                return (hash_table_retrieve(ht,limit - i), count)
-            print("Inside Outside", count,hash_table_retrieve(ht, limit - i))
-            return (count,hash_table_retrieve(ht,limit - i), count)
+                return (hash_table_retrieve(ht, limit - i), count)
+            print("Inside Outside", count, hash_table_retrieve(ht, limit - i))
+            return (count, hash_table_retrieve(ht, limit - i), count)
         print("COUNT", count)
         count += 1
 
